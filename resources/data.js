ec23e342d79841d3138a6c25adb94935b0babc53
unknown <38155769+timtam8520@users.noreply.github.com>
2018-09-02 17:27:01 +1000

    feat: adding empty bids resource and changing resources to maps

--- resources/data.js
+++ resources/data.js
@@ -1,54 +1,60 @@
 const users = [
-    {
-        userId: 1,
-        firstName: 'Bob',
-        lastName: 'Proctor',
-        userEmail: 'bob@proctor.com'
-    },
-    {
-        userId: 2,
-        firstName: 'Susie',
-        lastName: 'Jane',
-        userEmail: 'susie@jane.com'
-    },
-    {
-        userId: 3,
-        firstName: 'Peter',
-        lastName: 'Thompson',
-        userEmail: 'peter@thompson.com'
-    },
-    {
-        userId: 4,
-        firstName: 'Sally',
-        lastName: 'Christopher',
-        userEmail: 'sally@christopher.com'
-    }
+  {
+    userId: 1,
+    firstName: 'Bob',
+    lastName: 'Proctor',
+    userEmail: 'bob@proctor.com'
+  },
+  {
+    userId: 2,
+    firstName: 'Susie',
+    lastName: 'Jane',
+    userEmail: 'susie@jane.com'
+  },
+  {
+    userId: 3,
+    firstName: 'Peter',
+    lastName: 'Thompson',
+    userEmail: 'peter@thompson.com'
+  },
+  {
+    userId: 4,
+    firstName: 'Sally',
+    lastName: 'Christopher',
+    userEmail: 'sally@christopher.com'
+  }
 ];
 
 const products = [
-    {
-        productId: 1,
-        productName: 'Kettle',
-        productImage: 'https://www.kmart.com.au/wcsstore/Kmart/images/ncatalog/tf/6/42570936-1-tf.jpg'
-    },
-    {
-        productId: 2,
-        productName: 'Tennis Racquet',
-        productImage: 'https://www.tennisdirect.com.au/assets/full/17538-G2.jpg'
-    },
-    {
-        productId: 3,
-        productName: 'Monitor',
-        productImage: 'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/h/g/hg90_edit_4.png'
-    },
-    {
-        productId: 4,
-        productName: 'The Chronicles of Narnia Book Set',
-        productImage: 'https://i.ebayimg.com/images/g/yA8AAOSw2gxYtsx7/s-l300.jpg'
-    }
+  {
+    productId: 1,
+    productName: 'Kettle',
+    productImage: 'https://www.kmart.com.au/wcsstore/Kmart/images/ncatalog/tf/6/42570936-1-tf.jpg'
+  },
+  {
+    productId: 2,
+    productName: 'Tennis Racquet',
+    productImage: 'https://www.tennisdirect.com.au/assets/full/17538-G2.jpg'
+  },
+  {
+    productId: 3,
+    productName: 'Monitor',
+    productImage: 'https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/h/g/hg90_edit_4.png'
+  },
+  {
+    productId: 4,
+    productName: 'The Chronicles of Narnia Book Set',
+    productImage: 'https://i.ebayimg.com/images/g/yA8AAOSw2gxYtsx7/s-l300.jpg'
+  }
 ];
 
+const bids = [
+];
+
+const arrayToMap = (id, arr) => new Map(arr.map(item => [item[id], item]));
+
 module.exports = {
-    users,
-    products
+  users: arrayToMap('userId', users),
+  products: arrayToMap('productId', products),
+  productBids: arrayToMap('productId', bids)
 };
